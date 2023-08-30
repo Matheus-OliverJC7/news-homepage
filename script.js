530ae01843bcdb6eb8a80e903e4f50b9a42f9316
Matheus de Oliveira <eumatheusdeoliveirajc@gmail.com>
2023-08-30 18:23:31 -0300

    finalizado

---
+++ script.js
@@ -0,0 +1,8 @@
+function openMenu() {
+    let menu = document.querySelector('#menu-mobile')
+    menu.style.display = 'block'
+}
+function closeMenu() {
+    let menu = document.querySelector('#menu-mobile')
+    menu.style.display = 'none'
+}
